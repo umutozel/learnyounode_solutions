0355b79239d2daa7f30ed994556ca80d96206f76
Umut Ozel <umutozel@outlook.com>
2014-09-02 09:24:30 +0300

    Bug fix.
    Bug: Used to make uppercase when method is not post.

--- 12_http_uppercaserer.js
+++ 12_http_uppercaserer.js
@@ -6,9 +6,10 @@ var through2Map = require('through2-map');
 var server = http.createServer(function(request, response) {
 	if (request.method != 'POST')
 		response.end('Please use POST.');
-		
-	request.pipe(through2Map(function (data) {
-		return data.toString().toUpperCase();
-	})).pipe(response);
+	else {
+		request.pipe(through2Map(function (data) {
+			return data.toString().toUpperCase();
+		})).pipe(response);
+	}
 });
 server.listen(port);
